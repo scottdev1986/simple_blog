64565a57b1681a2151a6549659c8efcb9d501f91
Scott Kellar <scottdev1986@gmail.com>
2021-12-29 17:10:12 -0500

    adding admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'is_published', 'published_date')
+    list_display_links =('id', 'author')
+    list_editable = ('is_published',)
+    list_per_page = 25
+
+admin.site.register(Article, ArticleAdmin)
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Article(models.Model):
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    description = models.CharField(max_length=200, blank=True)
+    body = models.TextField()
+    is_published = models.BooleanField(default=False)
+    published_date = models.DateTimeField(default=datetime.now, blank=True)
