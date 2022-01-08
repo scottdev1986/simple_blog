1263342f1a88a2440fce6c406c2e1dfcab342b32
Scott <88298032+scottdev1986@users.noreply.github.com>
2022-01-08 12:52:53 -0500

    change settings

--- smblog/settings.py
+++ smblog/settings.py
@@ -77,6 +77,17 @@ WSGI_APPLICATION = 'smblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
